a5b6e5dad1c3a0c52f812bd0a85c9d01ffe9d780
Eugeny Dementev <eugeny.dementev@ya.ru>
2017-10-10 17:42:39 +0800

    add redux integration unit-tests

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,3 +1,5 @@
+const redux = require('redux');
+
 const Lab = require('lab');
 const lab = exports.lab = Lab.script();
 const { experiment, test } = lab;
@@ -66,5 +68,39 @@ experiment('createReducerSwitcher', () => {
 
     reducerSwitcher.switch('reducer');
     reducerSwitcher.reducer(...args);
+  });
+});
+
+experiment('Redux', () => {
+  let reducerSwitcher;
+  let store;
+
+  function reducerOne (state = {}, action) {
+    return 'one';
+  }
+
+  function reducerTwo (state = {}, action) {
+    return 'two';
+  }
+
+  test('should switch', (done) => {
+    reducerSwitcher = createReducerSwitcher({
+      one: reducerOne,
+      two: reducerTwo,
+    });
+
+    store = redux.createStore(redux.combineReducers({
+      value: reducerSwitcher.reducer,
+    }));
+
+    assert.strictEqual(store.getState().value, 'one');
+
+    reducerSwitcher.switch('two');
+
+    store.dispatch({ type: 'SOME_ACTION' });
+
+    assert.strictEqual(store.getState().value, 'two');
+
+    done();
   });
 });
